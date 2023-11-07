412c7ac23981c01eea6e93b08fac5d325df1de70
unknown <you@example.com>
2023-11-07 10:34:32 +0200

    פרםסט ורך

--- src/helpers/ServerRoutes.js
+++ src/helpers/ServerRoutes.js
@@ -1,8 +1,7 @@
 export const PROXY_URL = "http://localhost:5000";
-// export const CHOSEN_PROXY_URL = PROXY_URL;
 
 // src/helpers/ServerRoutes.js
 export const RENDER_PROXY_URL = "https://react-multi-match-game-production-05a5.up.railway.app"; // Use the full URL, including the protocol
 
-// export const CHOSEN_PROXY_URL = RENDER_PROXY_URL;
-export const CHOSEN_PROXY_URL = PROXY_URL;
+export const CHOSEN_PROXY_URL = RENDER_PROXY_URL;
+// export const CHOSEN_PROXY_URL = PROXY_URL;
