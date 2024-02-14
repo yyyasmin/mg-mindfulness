348342359d246ca08f6cf4637c66f7faf1d9f244
unknown <you@example.com>
2024-02-14 17:56:36 +0200

    url

--- src/helpers/ServerRoutes.js
+++ src/helpers/ServerRoutes.js
@@ -1,5 +1,5 @@
 export const PROXY_URL = "http://localhost:5000";
 export const RENDER_PROXY_URL = "share-and-listen-production.up.railway.app";
 
-//export const CHOSEN_PROXY_URL = RENDER_PROXY_URL;
-export const CHOSEN_PROXY_URL = PROXY_URL;
+export const CHOSEN_PROXY_URL = RENDER_PROXY_URL;
+//export const CHOSEN_PROXY_URL = PROXY_URL;
