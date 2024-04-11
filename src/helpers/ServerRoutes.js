79746ed080a0df86988fcb1bf9d01cfad807c332
unknown <you@example.com>
2024-04-11 19:40:05 +0300

    players2 url

--- src/helpers/ServerRoutes.js
+++ src/helpers/ServerRoutes.js
@@ -2,5 +2,5 @@ export const PROXY_URL = "http://localhost:5000";
 //export const RENDER_PROXY_URL = "https://share-and-listen-production.up.railway.app";  // main DEPLOY
 export const RENDER_PROXY_URL = "share-and-listen-production-d3bb.up.railway.app";  // players_timeout_2 DEPLOY
 
-//export const CHOSEN_PROXY_URL = RENDER_PROXY_URL;
-export const CHOSEN_PROXY_URL = PROXY_URL;
+export const CHOSEN_PROXY_URL = RENDER_PROXY_URL;
+//export const CHOSEN_PROXY_URL = PROXY_URL;
